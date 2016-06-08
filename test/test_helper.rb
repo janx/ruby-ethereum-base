04f9a388339a0c67c56ede9f4d4b9fd07c15ad54
Steve Ellis <email@steveell.is>
2016-06-08 01:00:00 -0400

    remove RSpec; use Minitest

---
+++ test/test_helper.rb
@@ -0,0 +1,9 @@
+$:.unshift File.expand_path('../lib', __FILE__)
+
+require 'minitest/autorun'
+require 'ethereum/base'
+require 'json'
+
+class Minitest::Test
+
+end
---
+++ test/utils_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class UtilsTest < Minitest::Test
+
+  def test_basics
+    assert_equal 1, 1
+  end
+
+end
