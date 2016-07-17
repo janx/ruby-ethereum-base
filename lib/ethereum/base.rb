4837682d91bd339a687f73627afaa9480a944e7b
Steve Ellis <email@steveell.is>
2016-07-17 12:44:41 -0400

    Add commonly used constants

--- lib/ethereum/base.rb
+++ lib/ethereum/base.rb
@@ -6,8 +6,18 @@ module Ethereum
 
     VERSION = "0.1.2"
 
+    BYTE_ZERO = "\x00".freeze
+    GTXCOST = 21000       # TX BASE GAS COST
+    GTXDATANONZERO = 68   # TX DATA NON ZERO BYTE GAS COST
+    GTXDATAZERO = 4       # TX DATA ZERO BYTE GAS COST
+    SECP256K1_N = 115792089237316195423570985008687907852837564279074904382605163141518161494337
+    UINT_MAX = 2**256 - 1
+
     autoload :Sedes, 'ethereum/base/sedes'
     autoload :Utils, 'ethereum/base/utils'
+
+    class ValidationError < StandardError; end
+    class InvalidTransaction < ValidationError; end
 
   end
 end
---
+++ lib/ethereum/base/version.rb
@@ -0,0 +1,7 @@
+module Ethereum
+  module Base
+
+    VERSION = "0.1.2"
+
+  end
+end
