c91f9765f3f66cf157eea3bccdd2770d12a6cbd8
Steve Ellis <email@steveell.is>
2016-07-22 17:58:43 -0400

    organize some of constants

--- lib/ethereum/base.rb
+++ lib/ethereum/base.rb
@@ -5,12 +5,9 @@ module Ethereum
   module Base
 
     BYTE_ZERO = "\x00".freeze
-    GTXCOST = 21000       # TX BASE GAS COST
-    GTXDATANONZERO = 68   # TX DATA NON ZERO BYTE GAS COST
-    GTXDATAZERO = 4       # TX DATA ZERO BYTE GAS COST
-    SECP256K1_N = 115792089237316195423570985008687907852837564279074904382605163141518161494337
-    UINT_MAX = 2**256 - 1
 
+    autoload :Gas, 'ethereum/base/gas'
+    autoload :Secp256K1, 'ethereum/base/secp256k1'
     autoload :Sedes, 'ethereum/base/sedes'
     autoload :Utils, 'ethereum/base/utils'
 
